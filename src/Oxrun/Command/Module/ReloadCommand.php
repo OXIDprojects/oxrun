6832cf3bdc503a46aa52f975bd509b75e48ab659
Tobi Matthaiou <225997+TumTum@users.noreply.github.com>
2021-04-20 16:03:25 +0200

    New feature: reload a module only if that allow in a deploy:module-activator configration yaml.
    Option name is `--based-on-config dev_module.yaml`

--- src/Oxrun/Command/Module/ReloadCommand.php
+++ src/Oxrun/Command/Module/ReloadCommand.php
@@ -9,15 +9,60 @@
 namespace Oxrun\Command\Module;
 
 use OxidEsales\EshopCommunity\Internal\Framework\Console\Executor;
+use OxidEsales\EshopCommunity\Internal\Transition\Utility\ContextInterface;
+use Oxrun\Core\OxrunContext;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\ConsoleOutput;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Yaml\Yaml;
 
 class ReloadCommand extends Command
 {
+    /**
+     * @var InputInterface
+     */
+    private $input = null;
+
+
+    /**
+     * @var ConsoleOutput
+     */
+    private $output = null;
+
+    /**
+     * @var OxrunContext OxrunContext
+     */
+    private $oxrunContext;
+
+    /**
+     * @var ContextInterface
+     */
+    private $context;
+
+    /**
+     * @var array
+     */
+    private $configYaml;
+
+    /**
+     * ReloadCommand constructor.
+     * @param OxrunContext $oxrunContext
+     */
+    public function __construct(
+        OxrunContext $oxrunContext,
+        ContextInterface $context
+    )
+    {
+        $this->oxrunContext = $oxrunContext;
+        $this->context = $context;
+
+        parent::__construct();
+    }
+
 
     /**
      * Configures the current command.
@@ -30,8 +75,29 @@ class ReloadCommand extends Command
             ->addArgument('module', InputArgument::REQUIRED, 'Module name')
             ->addOption('force-cache', 'f',InputOption::VALUE_NONE, 'cache:clear with --force option')
             ->addOption('skip-cache-clear', 's',InputOption::VALUE_NONE, 'skip cache:clear command')
+            ->addOption('based-on-config', 'c',InputOption::VALUE_REQUIRED, 'Checks if module is allowed to be reloaded based on the deploy:module-activator yaml file.')
         ;
     }
+
+    /**
+     *
+     */
+    protected function initialize(InputInterface $input, OutputInterface $output)
+    {
+        $this->input = $input;
+        $this->output = $output;
+
+        if ($input->getOption('based-on-config')) {
+            try {
+                $configYaml = $this->oxrunContext->getConfigYaml($input->getOption('based-on-config'));
+                $this->configYaml = Yaml::parse($configYaml);
+            } catch (\Exception $e) {
+                $this->output->getErrorOutput()->writeln("<error>[Error] [{$input->getOption('based-on-config')}] {$e->getMessage()}</error>");
+                exit(2);
+            }
+        }
+    }
+
 
     /**
      * Executes the current commandd
@@ -56,6 +122,11 @@ class ReloadCommand extends Command
             $argvInputClearCache->setOption('force', true);
         }
 
+        if ($this->isModuleAllowed() == false) {
+            $this->output->writeln("<comment>({$this->context->getCurrentShopId()}) '{$input->getArgument('module')}' skip module reload on {$input->getOption('based-on-config')} </comment>");
+            return 0;
+        }
+
         //Run Command
         if (!$skipCacheClear) {
             $clearCommand->execute($argvInputClearCache, $output);
@@ -68,6 +139,32 @@ class ReloadCommand extends Command
         $activateCommand->execute($argvInputActivate, $output);
 
         return 0;
+    }
+
+    /**
+     * @return bool
+     */
+    private function isModuleAllowed()
+    {
+        if (empty($this->input->getOption('based-on-config'))) {
+            return true;
+        }
+
+        $shopId = $this->context->getCurrentShopId();
+        $moduleId = $this->input->getArgument('module');
+
+        $whitelist = $this->configYaml['whitelist'][$shopId] ?? null;
+        $blacklist = $this->configYaml['blacklist'][$shopId] ?? null;
+
+        if ($whitelist !== null && array_search($moduleId, $whitelist) !== false) {
+            return true;
+        }
+
+        if ($blacklist !== null && array_search($moduleId, $blacklist) === false) {
+            return true; //module is NOT on blacklist so is allowed
+        }
+
+        return false;
     }
 
     /**
